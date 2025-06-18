2d253ba962f7657508b7f005bc468bbb08afcba7
Buvi07 <94307710+Buvi07@users.noreply.github.com>
2025-06-18 23:59:30 +0530

    Delete ### directory

--- ###/modules/metadata.py
+++
@@ -1,3 +0,0 @@
-name = 'Deep-Live-Cam'
-version = '1.9'
-edition = 'GitHub Edition'
--- ###/modules/utilities/status.py
+++
@@ -1,7 +0,0 @@
-import modules.ui as ui
-import modules.globals
-
-def update_status(message: str, scope: str = 'DLC.CORE') -> None:
-    print(f'[{scope}] {message}')
-    if not modules.globals.headless and callable(ui.update_status):  # Ensure ui.update_status is callable
-        ui.update_status(message)
